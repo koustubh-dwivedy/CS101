612adf69fac5dced1992d9c3845c27df3df028b9
Vedika Shetty <shetty.vedika@gmail.com>
2015-04-03 22:52:55 +0530

    Adding determinant fn

--- det.cpp
+++ det.cpp
@@ -1,1 +1,21 @@
+#include"main_header.h"
+double det(Matrix &A)
+{
+    if(A.Rows==A.Columns)
+    {
+    Matrix temporary(A.Rows,A.Columns,1);
+    temporary=choose_matrix(U,A);
+    double d=1;
+    for(int i=0; i<temporary.Rows;i++)
+    {
+        d=d*temporary.matrix[i][i];
+    }
+    return d;
+    }
+    else
+    {
+        cout<<"This is not a square matrix. Please provide appropriate inputs."<<endl;
+        return 0;
+    }
 
+}
